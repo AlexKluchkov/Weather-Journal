b5276d4d7bb89b8021aed4b838ef5af8df34e96c
Alexander Kluchkov <136575524+AlexKluchkov@users.noreply.github.com>
2025-09-11 09:47:41 +0300

    Update Weather_Forecast.py

--- Weather_Forecast.py
+++ Weather_Forecast.py
@@ -5,14 +5,32 @@ from datetime import datetime
 
 class Weather_Forecast(object):
     weather_API = "58e3b566e5bed0958072fca73566c81f"
-   
-    def get_weather_forecast(self, CITY):
+
+    def get_three_hours_weather_forecast(self, CITY):
         weather_url = f"https://api.openweathermap.org/data/2.5/forecast?q={CITY}&appid={self.weather_API}&units=metric&lang=en"
-        #One Call API
+        #
         response = requests.get(weather_url)
         data = response.json()
 
-        # fg
+        weather_forecast_date = []
+        weather_forecast_temperature = []
+        weather_forecast_description = []
+        for item in data["list"]:
+            date = datetime.fromtimestamp(item["dt"])
+            temp = item["main"]["temp"]
+            desc = item["weather"][0]["description"]
+            weather_forecast_date.append(f"{date}")
+            weather_forecast_temperature.append(f"{temp} C")
+            weather_forecast_description.append(f"{desc}")
+        return weather_forecast_date, weather_forecast_temperature, weather_forecast_description
+
+
+    def get_five_day_weather_forecast(self, CITY):
+        weather_url = f"https://api.openweathermap.org/data/2.5/forecast?q={CITY}&appid={self.weather_API}&units=metric&lang=en"
+        #
+        response = requests.get(weather_url)
+        data = response.json()
+        #
         forecast_by_day = defaultdict(list)
 
         for item in data["list"]:
@@ -21,9 +39,14 @@ class Weather_Forecast(object):
             desc = item["weather"][0]["description"]
             forecast_by_day[date].append((temp, desc))
         
-        five_day_weather_forecast = []
-        for date, values in list(forecast_by_day.items())[:5]:  # максимум 5 дней
-            avg_temp = sum(v[0] for v in values) / len(values)
+        five_day_weather_forecast_date = []
+        five_day_weather_forecast_temperature = []
+        five_day_weather_forecast_description = []
+        for date, values in list(forecast_by_day.items())[:5]:  # max 5 days
+            min_temp = min(v[0] for v in values)
+            max_temp = max(v[0] for v in values)
             descs = [v[1] for v in values]
-            five_day_weather_forecast.append(f"{date}: {avg_temp:.1f} C, {descs[0]}")
-        return five_day_weather_forecast
+            five_day_weather_forecast_date.append(f"{date}")
+            five_day_weather_forecast_temperature.append(f" {min_temp:.1f} C {max_temp:.1f} C")
+            five_day_weather_forecast_description.append(f"{descs[0]}")
+        return five_day_weather_forecast_date, five_day_weather_forecast_temperature, five_day_weather_forecast_description
